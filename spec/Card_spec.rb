cc604f2b9c63847e7ee96b0a294d4a3f56c40c8b
Elton Trujillo <eltontrujillo@outlook.com>
2024-03-29 21:24:04 -0500

    card spec is written

---
+++ spec/Card_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/Card'
+
+RSpec.describe Card do
+  let(:card) {Card.new('J', 'Spades')}
+  it "intialized" do
+    card = Card.new('J','Spades')
+
+  end
+
+  it 'reads suit' do
+    expect(card.suit).to eq('Spades')
+  end
+
+  it 'reads the value' do
+    expect(card.value).to eq(7)
+  end
+
+end
