536f728b1b77c31adf66f6af3f860e66169d6b79
Elton Trujillo <eltontrujillo@outlook.com>
2024-03-29 21:42:13 -0500

    added a intialize for deckspec

--- spec/Deck_spec.rb
+++ spec/Deck_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/Deck'
 
 RSpec.describe Deck do
-
+  describe '#initialize' do
+    it 'creates deck of 52 cards' do
+      deck = Deck.new
+      expect(deck.cards.length).to eq(52)
+    end
+  end
 end
